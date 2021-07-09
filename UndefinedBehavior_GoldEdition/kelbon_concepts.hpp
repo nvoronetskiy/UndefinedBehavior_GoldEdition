c271d1cee595bd905f956d91dc01c3c649c63488
kelbon <kelbonage@gmail.com>
2021-07-09 22:49:23 +0500

    delete not functor concept

--- UndefinedBehavior_GoldEdition/kelbon_concepts.hpp
+++ UndefinedBehavior_GoldEdition/kelbon_concepts.hpp
@@ -1,15 +1,14 @@
 
 #ifndef KELBON_CONCEPTS_HPP
 #define KELBON_CONCEPTS_HPP
+
 template<typename T> struct remove_pointer { using type = T; };
 template<typename T> struct remove_pointer<T*> { using type = T; };
 
 template<typename T> using remove_pointer_t = typename remove_pointer<T>::type;
 
 template<typename T>
-concept functor = requires { T::operator(); }; // todo - понять почему без noexcept это не работает
+concept functor = requires { T::operator(); };
 
-template<typename T>
-concept not_functor = !functor<T>;
-// мб убрать, это проверочка(костыльчик), todo - concept function
+// todo - concept function
 #endif // !KELBON_CONCEPTS_HPP
